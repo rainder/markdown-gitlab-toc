5c1632f433c2597ae843d53e631e5a8dd4b9f643
Andrius Skerla <andrius@skerla.com>
2015-02-11 22:09:21 +0000

    hello world

---
+++ bin/markdown-toc.js
@@ -0,0 +1,20 @@
+#!/usr/local/bin/iojs
+'use strict';
+
+var commander = require('commander');
+var co = require('co');
+var packageJson = require('./../package.json');
+
+commander
+  .version(packageJson.version)
+  .option('-o, --output <path>', 'Output to a file')
+  .option('-x, --exclude <path>', 'Exlude matches')
+  .parse(process.argv);
+
+
+co(function * () {
+  yield require('./../lib/markdown-toc')();
+}).catch(function (err) {
+  console.error(err.message);
+  console.error(err.stack);
+});
